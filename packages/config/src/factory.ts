a9e4d0087d17170b332fc84ad965c4bc92cb87d2
VLTHellolin <i@hellolin.top>
2025-10-07 20:56:51 +0800

    feat: auto detect node.js/bun to enable node rules

--- packages/config/src/factory.ts
+++ packages/config/src/factory.ts
@@ -1,6 +1,7 @@
 import type { ReactOptions } from '@hellolin-eslint/react-config';
 import type { UnoCSSOptions } from '@hellolin-eslint/unocss-config';
 import { type FlatConfigItem, requirePackage } from '@hellolin-eslint/shared';
+import { isPackageExists } from 'local-pkg';
 import { commands, type CommandsOptions } from './modules/commands';
 import { ignores, type IgnoresOptions } from './modules/ignores';
 import { imports } from './modules/imports';
@@ -56,21 +57,21 @@ export const defineConfig = async (options: ESLintConfigOptions = {}): Promise<F
     );
   }
 
-  if (options.node ?? false) {
+  if (options.node || isPackageExists('@types/node') || isPackageExists('@types/bun')) {
     const { node } = await requirePackage('@hellolin-eslint/node-config') as typeof import('@hellolin-eslint/node-config');
     configs.push(node());
   }
-  if (options.react ?? false) {
+  if (options.react) {
     const { react } = await requirePackage('@hellolin-eslint/react-config') as typeof import('@hellolin-eslint/react-config');
     configs.push(
       react(resolveOptions(options.react)),
     );
   }
-  if (options.storybook ?? false) {
+  if (options.storybook) {
     const { storybook } = await requirePackage('@hellolin-eslint/storybook-config') as typeof import('@hellolin-eslint/storybook-config');
     configs.push(storybook());
   }
-  if (options.unocss ?? false) {
+  if (options.unocss) {
     const { unocss } = await requirePackage('@hellolin-eslint/unocss-config') as typeof import('@hellolin-eslint/unocss-config');
     configs.push(unocss(resolveOptions(options.unocss)));
   }
