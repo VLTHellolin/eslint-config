6bd2c2f2a7269cc45a7945e65dbf056ab41673bd
VLTHellolin <i@hellolin.top>
2026-02-06 20:41:45 +0800

    fix: react compiler rules

--- packages/react-compiler/src/index.ts
+++ packages/react-compiler/src/index.ts
@@ -13,21 +13,8 @@ export const reactCompiler = (): FlatConfigItem[] => {
       /// keep-sorted
       rules: {
         'react-compiler/react-compiler': 'error',
-        'react-hooks/component-hook-factories': 'error',
-        'react-hooks/config': 'error',
-        'react-hooks/error-boundaries': 'error',
-        'react-hooks/gating': 'error',
-        'react-hooks/globals': 'error',
-        'react-hooks/immutability': 'error',
-        'react-hooks/incompatible-library': 'warn',
         'react-hooks/preserve-manual-memoization': 'error',
-        'react-hooks/purity': 'error',
-        'react-hooks/refs': 'error',
-        'react-hooks/set-state-in-effect': 'error',
-        'react-hooks/set-state-in-render': 'error',
-        'react-hooks/static-components': 'error',
         'react-hooks/unsupported-syntax': 'warn',
-        'react-hooks/use-memo': 'error',
       },
     },
   ];
